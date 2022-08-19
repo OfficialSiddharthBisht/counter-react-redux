469b994a5dad136f695c4ed31afeb4cf8cf18685
Siddharth <officialsiddharthbisht@gmail.com>
2022-08-19 23:15:48 +0530

    ADD: made button and added its css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ function App() {
   return (
     <div className="App">
       <h1>Counter Application React-Redux</h1>
+      <div className='container'>
+        <button><span>-</span></button>
+        <input className = "text" type="text" value="0"></input>
+        <button><span>+</span></button>
+      </div>
     </div>
   );
 }
